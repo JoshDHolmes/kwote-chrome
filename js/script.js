20b5e1fe5385c3eb74dc613d2ca8f428e958dd93
Joshua Holmes <hello@joshuaholmes.id.au>
2014-09-24 20:50:34 +1000

    Updated script and HTML
    Nicer fade effect and patched an error where non-movie quotes were
    displaying.

--- js/script.js
+++ js/script.js
@@ -1,17 +1,28 @@
+$(document).ready(function start(){
     $.ajax({
-          url: 'https://andruxnet-random-famous-quotes.p.mashape.com/cat=movies',
-          type: 'GET',
-          dataType: 'json',
-          success: function(quote) { 
-              console.log(quote);
-              $('#text h1').text(quote.quote);
-              $('#text p').text("- "+quote.author);
-                                   },
-          beforeSend: setHeader
-        });
+        url: 'https://andruxnet-random-famous-quotes.p.mashape.com/cat=movies',
+        type: 'GET',
+        dataType: 'json',
+        success: function(quote) {
+            var cat = quote.category;
+            console.log(quote);
+            if (cat != "Movies") {
+                start();
+                return;
+            }
+            else {
+                $('#text h1').text(quote.quote).fadeIn(800);
+                $('#text p').text("- "+quote.author).fadeIn(800);
+            }
+
+        },
+
+        beforeSend: setHeader
+    });
+})
 
 function setHeader(xhr) {
         xhr.setRequestHeader('X-Mashape-Key', 'j2Lg2CepGhmshmXFj70zdWgIkoGzp1OSVZvjsnLoSBzQjYcegB');
       }
 
-//This script relies on APIs from Mashape.
+//This script relies on APIs from Mashape (http://mashape.com/andruxnet/random-famous-quotes).
